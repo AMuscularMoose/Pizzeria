f324c70687c44541c821741d3b50cae8dece097a
Justin Jang <T_Jang@baylor.edu>
2021-03-15 14:17:40 -0500

    3

--- Review/dictionaries.py
+++ Review/dictionaries.py
@@ -32,3 +32,12 @@ for k,v in fav_langs.items():
 for name in fav_langs.keys():
     print(name.title())
     #keys() method returns keys and the title() function tidies up the output
+
+phonebook = {'Chris':'555−1111',
+             'Katie':'555−2222',
+             'Joanne':'555−3333'}
+
+for key, value in phonebook.items():
+    print('KEY:',key,'VALUE:', value)
+for phonebook_tuple in phonebook.items():
+    print(phonebook_tuple)
