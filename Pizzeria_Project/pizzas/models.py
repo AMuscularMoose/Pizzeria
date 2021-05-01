d80cb7f77f04f7738cbe382d2f3c183d44149e09
Justin Jang <T_Jang@baylor.edu>
2021-04-30 23:41:17 -0500

    Finishing everything up until user registration

--- Pizzeria_Project/pizzas/models.py
+++ Pizzeria_Project/pizzas/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Pizza(models.Model):
     name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
@@ -12,6 +13,15 @@ class Pizza(models.Model):
 class Topping(models.Model):
     pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
